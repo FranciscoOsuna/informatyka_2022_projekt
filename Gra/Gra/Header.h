09c113e992a526e63e485abe44abc153b1c7b8c4
FranciscoOsuna <47285782+FranciscoOsuna@users.noreply.github.com>
2022-12-16 23:13:34 +0100

    Re-added movement
    also added a temporary enemy list to demonstrate the game's capabilities

--- Gra/Gra/Header.h
+++ Gra/Gra/Header.h
@@ -55,6 +55,11 @@ private:
 class Tanks
 {
 public:
+	float speed = 0.1;
+
+	sf::RectangleShape bodyRect;
+	sf::RectangleShape gunRect;
+	sf::CircleShape gunCircle;
 
 	Tanks(sf::Vector2f position, float orient = 0,
 		float sizeMult = 1,
@@ -84,7 +89,7 @@ public:
 
 		// Set the parameters of the base of the gun
 		gunCircle.setRadius(size.x / 3);
-		gunCircle.setOrigin(size / 3.0f);
+		gunCircle.setOrigin(size.x / 3, size.y / 6);
 		gunCircle.setPosition(position);
 		gunCircle.setFillColor(color2);
 		gunCircle.setOutlineColor(sf::Color::Black);
@@ -92,15 +97,26 @@ public:
 		gunCircle.setRotation(orient);
 
 	}
+	sf::Vector2f giveBodyPosition()
+	{
+		return bodyRect.getPosition();
+	}
+
+
+	float rotationFromVectorDifference(sf::Vector2f v1, sf::Vector2f v2)
+	{
+		float angle = (atan2((v2.y - v1.y), (v2.x - v1.x)) * 180 / 3.14159) - 90;
+		return angle;
+	}
 
 	sf::Vector2f vectorFromRotation(sf::RectangleShape& body) // Returns a vector according to body orientation
 	{
-		float rotation = body.getRotation() * 3.14159 / 180;
-		sf::Vector2f out = sf::Vector2f(0.02 * cos(rotation), 0.02 * sin(rotation));
+		float rotation = body.getRotation() * 3.14159/180 - 1.57079632679;
+		sf::Vector2f out = sf::Vector2f(cos(rotation),sin(rotation));
 		return out;
 	}
 
-	void draw(sf::RenderWindow& window)
+	virtual void draw(sf::RenderWindow& window)
 	{
 		// Draw the shapes to the window in the desired order
 		window.draw(bodyRect);
@@ -108,8 +124,90 @@ public:
 		window.draw(gunCircle);
 	}
 
-private:
-	sf::RectangleShape bodyRect;
-	sf::RectangleShape gunRect;
-	sf::CircleShape gunCircle;
+	
+};
+
+
+class Enemy : public Tanks
+{
+public:
+
+	Enemy(sf::Vector2f position, float orient = 0, float sizeMult = 1,
+		sf::Color color1 = sf::Color::Cyan, sf::Color color2 = sf::Color::Blue)
+		: Tanks(position, orient, sizeMult, color1, color2) {}
+
+	void fixTurretOn(Tanks target)
+	{
+		float orient = rotationFromVectorDifference(gunCircle.getPosition(), target.giveBodyPosition());
+		gunRect.setRotation(orient);
+	}
+
+	void draw(sf::RenderWindow& window) override
+	{
+		Tanks::draw(window);
+	}
+};
+
+
+class Player : public Tanks
+{
+public:
+	Player(sf::Vector2f position, float orient = 0, float sizeMult = 1,
+		sf::Color color1 = sf::Color::Cyan, sf::Color color2 = sf::Color::Blue)
+		: Tanks(position, orient, sizeMult, color1, color2) {}
+
+	void turretControl(sf::RenderWindow& window)
+	{
+		sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window));
+		float orient = rotationFromVectorDifference(gunCircle.getPosition(), mousePos);
+		gunRect.setRotation(orient);
+	}
+
+	void draw(sf::RenderWindow& window) override
+	{
+		turretControl(window);
+		
+		Tanks::draw(window);
+	}
+
+
+	void manageMovement(sf::Time deltaTime)
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			speed = 0.1;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			speed = 0.2;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			sf::Vector2f movementVector = vectorFromRotation(bodyRect);
+			gunCircle.move(speed * deltaTime.asMilliseconds() * movementVector);
+			bodyRect.move(speed * deltaTime.asMilliseconds() * movementVector);
+			gunRect.move(speed * deltaTime.asMilliseconds() * movementVector);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			sf::Vector2f movementVector = -vectorFromRotation(bodyRect);
+			gunCircle.move(speed * deltaTime.asMilliseconds() * movementVector);
+			bodyRect.move(speed * deltaTime.asMilliseconds() * movementVector);
+			gunRect.move(speed * deltaTime.asMilliseconds() * movementVector);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			bodyRect.rotate(-speed * deltaTime.asMilliseconds()*0.6);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			bodyRect.rotate(speed * deltaTime.asMilliseconds() * 0.6);
+		}
+	}
+
 };
--- Gra/Gra/Source.cpp
+++ Gra/Gra/Source.cpp
@@ -16,13 +16,45 @@ int main()
 {
 
 	Titles title;
-	sf::Clock titleTimer;
 
-	sf::RenderWindow window(sf::VideoMode(1600, 800), "[*]", sf::Style::Close); //create window
+	//create window
+	sf::RenderWindow window(sf::VideoMode(1600, 800), "[*]", sf::Style::Close);
 
-	Tanks Player(sf::Vector2f(400, 400)); // Creates the player tank
-	Tanks Enemy1(sf::Vector2f(800, 400), 180, 1.5, sf::Color::Red, sf::Color(245, 51, 126)); // Creates enemy tank
+	// Set the frame rate to 60 fps
+	window.setFramerateLimit(60);
 
+	// Turn off repeat keypress events on hold
+	window.setKeyRepeatEnabled(false);
+
+	// Get the current time
+	sf::Clock clock;
+	sf::Time elapsedTime;
+
+	// Create the player tank
+	Player player(sf::Vector2f(400, 400));
+
+	// Create the enemy tank
+
+	Enemy enemyList[16] =
+	{
+		Enemy(sf::Vector2f(200, 400), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(400, 200), 120, 0.5, sf::Color::Red, sf::Color(245, 0, 126)),
+		Enemy(sf::Vector2f(600, 600), 180, 2, sf::Color::Green, sf::Color(245, 51, 0)),
+		Enemy(sf::Vector2f(800, 400), 270, 0.8, sf::Color::Yellow, sf::Color(245, 51, 126)),
+		Enemy(sf::Vector2f(1000, 100), 180, 1.5, sf::Color::Red, sf::Color(50, 51, 120)),
+		Enemy(sf::Vector2f(50, 400), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 450), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 460), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 470), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 480), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 490), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 500), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 510), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 520), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(200, 530), 90, 1, sf::Color::Blue, sf::Color(0, 51, 126)),
+		Enemy(sf::Vector2f(1400, 400), 0, 9, sf::Color::Red, sf::Color(245, 0, 126)),
+	};
+	
 	while (window.isOpen()) //Game Loop
 	{
 		sf::Event event;
@@ -32,13 +64,22 @@ int main()
 				window.close();
 		}
 
+		// Get the elapsed time since the last frame
+		elapsedTime = clock.restart();
+
 		window.clear();
 
-		Player.draw(window);
-		Enemy1.draw(window);
+		for (int i = 0; i < 16; i++)
+		{
+			enemyList[i].fixTurretOn(player);
+			enemyList[i].draw(window);
+		}
+		
+
+		player.manageMovement(elapsedTime);
+		player.draw(window);
 
 		window.display();
-
 
 		title.titleAnim(window);
 	}
